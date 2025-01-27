9ba28632a621689c3632a34c5fdb014ad4f68bae
Adam Wasielewski <adam.wasielewski@gmail.com>
2025-01-27 12:07:13 +0100

    init

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    using Javonet.Clr.Sdk;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World from .NET Framework code!");
+
+            RuntimeContext context = Javonet.WithConfig("./javonetconf.json").Netcore("dockerimage");
+
+            string libraryPath = "/app";
+            string className = "WebApi.TestClass";
+            // string staticClassName = "WebApi.TestStaticClass";
+            try
+            {
+                context.LoadLibrary(libraryPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+                // load custom library
+             //   context.LoadLibrary(libraryPath);
+
+            // get type from the runtime
+            var calledRuntimeType = context.GetType(className).Execute();
+            var instance = calledRuntimeType.CreateInstance().Execute();
+            instance.InvokeInstanceMethod("TestMethod").Execute();
+
+            // var calledRuntimeTypeForStatic = calledRuntime.GetType(staticClassName).Execute();
+            // calledRuntimeTypeForStatic.InvokeStaticMethod("TestMethod").Execute();
+        }
+    }
+}
---
+++ NetCoreWebApplication/WebApi/WebApi/TestClass.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi
+{
+    public class TestClass
+    {
+        public string TestMethod(string name)
+        {
+            return $"Hello from .NET 8.0 class, {name}!";
+        }
+    }
+}
